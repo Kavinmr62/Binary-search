93cbef57b4365e2b36d275d8ff362b534adae23d
Kavin M R <154052561+Kavinmr62@users.noreply.github.com>
2025-05-28 06:57:41 +0530

    Update and rename README.md to binarysearch.java

---
+++ binarysearch.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int key=sc.nextInt();
+        int a[]=new int[100];
+        int left=0;
+        int right=n-1;
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();        
+        }
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(a[mid]==key){
+                System.out.println("Index of the key: "+mid);
+                break;
+            }
+            else if(a[mid]<key){
+                left=mid+1;
+            }
+            else{
+                right=mid-1;
+            }
+            
+        }
+        if(left>right){
+            System.out.println("Element not found");
+        }
+}}
